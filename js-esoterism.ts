8b758557e4458d9671c699ff382f8c8412da81df
FabriceT <fabrice.tsiava-prestataire@laposte.fr>
2022-07-07 15:31:45 +0200

    doc

--- js-esoterism.ts
+++ js-esoterism.ts
@@ -1,19 +1,19 @@
 
     // constructor called as a function
     const add = Function('a', 'b', 'return a + b')
-    console.log('add is a', typeof add)
+    console.log('add is a', typeof add) // add is a function
     
     // constructor called as constructor -> returns an object 
     const addObj = new Function('a', 'b', 'return a + b')
-    console.log('addObj is a', typeof add)
+    console.log('addObj is a', typeof add) // addObj is a function
 
     
-    console.log(add(1, 2))
-    console.log(addObj(1, 2))
+    console.log(add(1, 2)) // 3
+    console.log(addObj(1, 2)) // 3
 
     const ten = Number('10')
 
     const tenObj = new Number('10')
 
-    console.log('ten is a', typeof ten)
-    console.log('tenObj is a', typeof tenObj)
+    console.log('ten is a', typeof ten) // ten is a number
+    console.log('tenObj is a', typeof tenObj) // tenObj is a object
