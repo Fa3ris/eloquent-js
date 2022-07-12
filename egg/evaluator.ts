e183ab6af76b1abd273dfbe04c25aa9b4777b0e1
FabriceT <fabrice.tsiava-prestataire@laposte.fr>
2022-07-12 16:27:08 +0200

    evaluator first version

--- egg/evaluator.ts
+++ egg/evaluator.ts
@@ -1,19 +1,68 @@
 import { ApplyExpression, Expression, parse, ValueExpression, WordExpression } from "./parser";
 
+/* 
+    Terminology: expression vs statement
+
+    an “expression” is a combination of values and functions 
+    that are combined and interpreted by the compiler to create a new value, 
+    
+    as opposed to a “statement” which is just a standalone unit of execution and doesn’t return anything.
+    
+    One way to think of this is that the purpose of
+    an expression is to create a value (with some possible side-effects), 
+    
+    while the sole purpose of a statement is to have side-effects.
+
+*/
 
 type Scope = { [key: string]: EvaluationResult}
 
-type EvaluationResult = string | number | Function
+type EvaluationResult = string | number | boolean | Function
 
 type KeywordFunctions = {[key: string]: KeywordFunction }
 
 type KeywordFunction = (args: Expression[], scope: Scope) => EvaluationResult
 const keywordFunctions: KeywordFunctions = {}
 
-keywordFunctions['if'] = () => 10
-keywordFunctions['while'] = () => 20
-keywordFunctions['do'] = () => 30
-keywordFunctions['define'] = () => 40
+keywordFunctions['if'] = (args: Expression[], scope: Scope) => {
+
+    if (!args || args.length != 3) { throw SyntaxError("'if' expression requires 3 arguments but got " + (args? args.length : 0)) }
+
+    if (evaluateExpression(args[0], scope) !== false) {
+        return evaluateExpression(args[1], scope)
+    } else {
+        return evaluateExpression(args[2], scope)
+    }
+}
+keywordFunctions['while'] = (args: Expression[], scope: Scope) => {
+    if (!args || args.length != 2) { throw SyntaxError("'while' expression requires 2 arguments but got " + (args? args.length : 0)) }
+    
+    while(evaluateExpression(args[0], scope) !== false) {
+        evaluateExpression(args[1], scope)
+    }
+
+    return false
+}
+keywordFunctions['do'] = (args: Expression[], scope: Scope) =>{ 
+    
+    let lastEvaluation: EvaluationResult = false
+
+    for (let arg of args) {
+        lastEvaluation = evaluateExpression(arg, scope)
+    }
+
+    return lastEvaluation 
+}
+keywordFunctions['define'] = (args: Expression[], scope: Scope) => {
+    if (args.length != 2 || args[0].type != 'word') {
+        throw new SyntaxError("'define' requires 2 args but got " + args.length)
+    }
+
+    const value = evaluateExpression(args[1], scope)
+
+    scope[args[0].value] = value
+    return value
+ }
 
 function evaluateExpression(expression: Expression, scope: Scope): EvaluationResult {
 
@@ -52,10 +101,22 @@ function evaluateExpression(expression: Expression, scope: Scope): EvaluationRes
     return 0
 }
 
+const topScope: Scope = {}
+
+topScope['false'] = false;
+topScope['true'] = true;
+
+for (let operation of ['+', '-', '*', '/', '==', "<", ">"]) {
+    topScope[operation] = Function('a', 'b', `return a ${operation} b`)
+}
+
+topScope['print'] = (a: any) => { console.log(a); return a }
+Object.freeze(topScope)
+
 
 function evaluate(s: string): EvaluationResult {
-    const scope: Scope = {}
-    scope['x'] = 10
+    const scope: Scope = Object.create(topScope)
+    // scope['x'] = 10
     return evaluateExpression(parse(s), scope)
 }
 
@@ -63,9 +124,32 @@ function evaluate(s: string): EvaluationResult {
 // ----- TEST ------
 
 if (require.main === module) // if was called via node and not reported
-for (let p of ['123', '"hello"', '"hello"    ', '"fefefe', 'x', 'y', 'if(>(x, 2), 1, 2)',
-'while(>(x, 2), 1, 2)', 'do(>(x, 2), 1, 2)', 'define(>(x, 2), 1, 2)' ]) {
+for (let p of ['123', '"hello"', '"hello"    ', '"fefefe', 'x', 'y', 
 
+'if(10, 1, 2)',
+'if (0, 1, 2)',
+'if(>(x, 2), 1, 2)',
+'while(false, 1)',
+'do(>(x, 2), 1, 2)',
+'define(>(x, 2), 1, 2)',
+'define(toto, 1)',
+'do(define(toto, false), toto)',
+'if (true, false, "")',
+`if (
+    >(3, 0),
+    "yes", "no")`,
+
+    `
+    do(define(total, 0),
+       define(count, 1),
+       while(<(count, 11),
+             do(define(total, +(total, count)),
+                define(count, +(count, 1)))),
+       print(total))
+    ` // 55
+]) {
+
+    console.log('evaluate expression ' + p.replace(/(?:\r\n|\r|\n|\s{2,})/g, ''))
     try {
         console.dir(evaluate(p), {depth: null})
     } catch (err) {
