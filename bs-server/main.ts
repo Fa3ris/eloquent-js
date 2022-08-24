d4657b9ebf80b316d964b1fc1400165bcef8be16
Fa3ris <Fa3ris@users.noreply.github.com>
2022-08-24 14:45:30 +0200

    messing with node

--- bs-server/main.ts
+++ bs-server/main.ts
@@ -2,6 +2,8 @@ import chalk from 'chalk';
 
 import {readFile, writeFile} from 'fs'
 
+import {createServer, request} from 'http'
+
 console.log(chalk.blue('Hello server!'));
 console.log('args are', chalk.blue(process.argv));
 
@@ -20,3 +22,42 @@ writeFile('out/msg.txt', 'sending msg', {
     flag: 'a'},  (err) => {
     console.log('write error', err)
 })
+
+createServer((req, res) => {
+
+    res.writeHead(200, {'Content-Type': "text/html"})
+    res.write('<h1>Hello from server</h1>')
+    res.write(`<p>url requested: <b>${req.url}<b></p>`)
+    res.end()
+
+}).listen(8080)
+
+
+request({
+
+    host: 'eloquentjavascript.net',
+    path: '/20_node.html',
+    method: 'GET',
+    headers: {Accept: 'text/html'},
+},
+
+res => {
+
+    console.log('status', res.statusCode)
+    res.on('data', (data) => {
+        console.log('data received', data, String(data))
+    })
+
+    res.on('readable', () => {
+        console.log('stream is readable', res.read())
+    })
+
+    res.on('close', () => {
+        console.log('close stream')
+    })
+
+    res.on('end', () => {
+        console.log('end stream')
+    })
+
+} ).end()
